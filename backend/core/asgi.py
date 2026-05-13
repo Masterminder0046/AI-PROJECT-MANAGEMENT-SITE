3f9ae3c367c8fb7d20945fb22b7cdefc75f41356
Sheik_Mohamed <167944109+Masterminder0046@users.noreply.github.com>
2026-05-13 09:41:31 +0530

    Update asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,10 +1,10 @@
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-django.setup()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_asgi_application()
 
 from websocket.routing import websocket_urlpatterns  # noqa: E402
 from websocket.middleware import JWTAuthMiddlewareStack  # noqa: E402
