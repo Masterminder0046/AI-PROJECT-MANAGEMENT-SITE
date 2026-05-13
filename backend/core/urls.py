e0e9b80f72d427bc408f080a5e437e111dcb1c1e
Sheik_Mohamed <167944109+Masterminder0046@users.noreply.github.com>
2026-05-13 11:11:51 +0530

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
+from django.shortcuts import redirect
+HttpResponse import
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.v1.urls")),
+    path('', lambda request: HttpResponse("Synapse AI SaaS Running 🚀")),
+    path('', lambda request: redirect('/pages/dashboard.html')),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
